fb63dacf4fa64985ae930dfdcf1677a9052a7780
Sandro Simperl <sandrosimperl.cc@gmail.com>
2023-01-25 15:08:45 +0100

    updated content

--- 06_Zahlenspiel_A.py
+++ 06_Zahlenspiel_A.py
@@ -1,41 +1,60 @@
+#
 # Spiel Aufgabe
+#
 
+#
 # import libs
 import random
 import os
 
+#
 # set vars
 s = 100
 e = 400
 t = 9
 r = t - 4
 
+#
 # clearing the output
 def clear():
     os.system( "cls" )
 
+#
 # set random nnuber
 def random_number( s, e ):
     return random.randint( s, e )
 
+#
 # get users input between the correct range
 def users_input( s, e ):
     return input( "\nBitte geben Sie eine Zahl zwischen " + str( s ) + " und " + str( e ) + " ein:" )
 
-# wrong_out
-def wrong_out( a, x, y ):
+#
+# check users input for int or char
+def check_input( s, e, a, r ):
+    return False
+
+#
+# output if wrong numbers
+def wrong_out_1( a, x, y ):
     clear()
     print( "Weiter viel Spass am Spielen.\nDebug: " + str( x ) + "\n" )
     print( "Zahl ist nicht richtig, Sie liegen " + str( abs( x - y ) ) + " daneben" )
     print( "Sie haben noch " + str( a ) + " Versuche" )
 
-# wrong_out
 def wrong_out_2( a, x, y ):
     clear()
     print( "Weiter viel Spass am Spielen.\nDebug: " + str( x ) + "\n" )
-    print( "Zahl ist nicht richtig, Sie liegen " + ( "darüber" if x > y else "darunter" ) )
+    print( "Zahl ist nicht richtig, Sie liegen " + ( "darüber" if x < y else "darunter" ) )
     print( "Sie haben noch " + str( a ) + " Versuche" )
 
+#
+# check the tries
+def check_try():
+    return False
+
+
+#
 # try again
 def try_again( s, e, t, r ):
     c = input( "Wollen Sie nocheinmal spielen? [ j/n | y/n ]" )
@@ -45,8 +64,10 @@ def try_again( s, e, t, r ):
         clear()
         print( "Danke, Das Spiel wird beendet\n" )
 
+#
 # start the game
 def start_game( s, e, t, r ):
+    #
     # get informations
     clear()
     print( "In diesem Spiel wird eine zufällige Zahl generiert zwischen " + str( s ) + " und " + str( s ) + "." )
@@ -55,17 +76,21 @@ def start_game( s, e, t, r ):
     print( "Als Erleichterung bekommen Sie ab dem " + str( r ) + ". Versuch auch gesagt, ob Sie über oder unter der Zahl liegen." )
     print( "\nAlso viel Spass am Spielen." )
 
+    #
     # get random number
     x = random_number( s, e )
     print( "Debug: " + str( x ) + "\n" )
 
+    #
     # set tries as a
     a = t
 
+    #
     # loop for tries and answers
     while True:
         y = users_input( s, e )
 
+        #
         # check if input is int or char
         if y.isdigit():
             y = int( y )
@@ -79,6 +104,7 @@ def start_game( s, e, t, r ):
             else:
                 continue
 
+        #
         # if no other tries left
         if a == 0:
             clear()
@@ -92,11 +118,12 @@ def start_game( s, e, t, r ):
                 continue
             # if number does not fits the random number
             if y != x:
-                wrong_out( a, x, y )
+                wrong_out_1( a, x, y )
                 a -= 1
                 continue
             # if number fits the random number
             else:
+                clear()
                 print( "Bravo, Sie haben die Zahl " + str( x ) + " erraten" )
                 try_again( s, e, t, r )
                 break
@@ -117,5 +144,6 @@ def start_game( s, e, t, r ):
                 try_again( s, e, t, r )
                 break
         break
-
+#
+# start the game
 start_game( s, e, t, r)
